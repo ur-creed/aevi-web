1c3513a56c374dbd1cb64f7a5649af9b5b029738
Chris Polen <christopher@aeviweb.com>
2021-11-12 15:45:09 -0500

    Took away Aevi Lettering in the center, Worked more on the nav..added logo as well

--- index.php
+++ index.php
@@ -15,33 +15,28 @@
 </head>
 
 <body class="body">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#"><img src="images/jara.png" alt="Aevi Web Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#"><i class="fas fa-user-friends"></i> About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#"><i class="fas fa-tags"></i> Pricing</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="#"><i class="fas fa-id-card"></i> Contact</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-<div class="container">
-<h1>Aevi Web</h1>
-    <hr>
-    <p><i>"Where possibilities are infinite"</i></p>
-</div>
-
+    
 <!-- Bootstrap JS CDN -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
