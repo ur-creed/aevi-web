7506126d5402154a8fc4981b0fa4674a7c91645a
Chris Polen <polen.chris89@gamil.com>
2021-11-14 20:17:49 -0500

    moved nav to nav.php

--- index.php
+++ index.php
@@ -14,27 +14,7 @@
 </head>
 
 <body class="body">
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="images/jara.png" alt="Aevi Web Logo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-user-friends"></i> About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-tags"></i> Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-id-card"></i> Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include 'nav.php'?>
     
 <!-- Bootstrap JS CDN -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
