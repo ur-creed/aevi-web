fe6c05c359efed326332c2addadf44812a1b5366
Creed <creed@aeviweb.com>
2021-11-16 11:34:13 -0500

    Updated background and navigation. Added moving particles to background.

--- index.php
+++ index.php
@@ -5,40 +5,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
     <title>Aevi Web | Infinite Possibilities</title>
-<!-- Bootstrap CSS CDN -->
+	
+	<!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<!-- Font Awesome CDN -->
+	<!-- Font Awesome CDN -->
     <script src="https://kit.fontawesome.com/8545886103.js" crossorigin="anonymous"></script>
-<!-- Local CSS -->
+	<!-- Local CSS -->
     <link rel="stylesheet" href="css/index.css" type="text/css">
-
 </head>
-
 <body class="body">
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="images/jara.png" alt="Aevi Web Logo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-user-friends"></i> About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-tags"></i> Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-id-card"></i> Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-    
-<!-- Bootstrap JS CDN -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<!-- The navigation bar -->
+	<?php include_once 'navigation.php' ?>
+	<!-- Particles in the background -->
+	<?php include_once 'particles.php' ?>
+	
+	<!-- Bootstrap JS CDN -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
