603af7b3a08bb51dd8d082beccb5cf292dbce681
Creed <creed@aeviweb.com>
2021-11-18 13:14:42 -0500

    Updated Fonts, padding, and size. Added initial content to about.

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
     <title>Aevi Web | Infinite Possibilities</title>
-	
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Archivo" rel="stylesheet">
 	<!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<!-- Font Awesome CDN -->
--- navigation.php
+++ navigation.php
@@ -7,13 +7,13 @@
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav ms-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="#">About</a>
+					<a class="nav-link" href="about.php">About</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Pricing</a>
+					<a class="nav-link" href="pricing.php">Pricing</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Contact</a>
+					<a class="nav-link" href="">Contact</a>
 				</li>
 			</ul>
 		</div>
