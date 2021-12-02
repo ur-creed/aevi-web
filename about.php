f6e0de9ec7baaf5a3b32b6f7735a58685da33f80
Creed <creed@aeviweb.com>
2021-12-02 13:50:11 -0500

    Updated content wording. Added footer and navigation.

--- about.php
+++ about.php
@@ -17,49 +17,42 @@
     <!-- Local CSS -->
     <link rel="stylesheet" href="css/about.css" type="text/css">
     <!-- Local JS -->
-    <script src="js/index.js" type="text/javascript"></script>
-    <title>About Us</title>
+    <script src="js/about.js" type="text/javascript"></script>
+    <title>Aevi Web | About Us</title>
 </head>
 <body>
-<!-- The navigation bar -->
-<?php include_once 'navigation.php' ?>
-<div class="container container-header bg-white p-5">
-<h1 class="display-4 mt-0">About Us</h1>
-    <hr class="my-4">
-    <a href="#" class="btn btn-primary btn-lg" role="button">Contact Us</a>
-</div>
-<div class="container container-about">
-	<div class="row row-content">
-            <p class="display-6 text-center text-white col-lg-6 col-sm-12">
-                At Aevi Web, we believe everyone single human being deserves to have their dreams come to life.
-                If you WordPress Squarespace,
-                We build solutions for all your website needs whether it's fully custom built or using a CMS (Content Management System).
-                We are here to be the starlight guiding you on your web presence journey. We believe in
-                <strong>Creating</strong> / <strong>Maintaining</strong> / <strong>Consulting</strong> with you every step of the way until your web dream becomes a reality and beyond.
+	<!-- The navigation bar -->
+	<?php include_once 'navigation.php' ?>
+	<div class="container container-header bg-white p-5">
+		<h1 class="display-4 mt-0">About Us</h1>
+		<hr class="my-4">
+		<a href="#" class="btn btn-primary btn-lg" role="button">Contact Us</a>
+	</div>
+	<div class="container container-about">
+		<div class="row row-content">
+			<p class="display-6 text-center text-white col-lg-6 col-sm-12">
+	            At Aevi Web, we believe every human being deserves to have their dreams come to life.
+	            Whether you have an existing idea or project in place and need updates or want a brand spanking new project built, we can help with that!
+	            We work with custom websites, WordPress, Squarespace, Magento, or just about any other CMS (Content Management System) too!
+	            From basic to advanced, we are here to build you the best website out there!
+	            <br>
+	            <strong>Creating</strong> / <strong>Maintaining</strong> / <strong>Consulting</strong>
             </p>
-        <div class="col-sm d-none d-lg-block">
-                <img src="images/Aevi%20Web.png"  class="animate__animated animate__slideInRight img-fluid" height="500" width="500" alt="logo">
-        </div>
+		
+	        <div class="col-sm d-none d-lg-block">
+	                <img src="images/Aevi%20Web.png"  class="animate__animated animate__slideInRight img-fluid" height="500" width="500" alt="logo">
+	        </div>
 
             <p class="p-3 display-6 text-center text-white col-12">
-                As experienced developers and brothers based out of Central Virginia with a passion for creating web solutions,
-                we decided to harness that fire by changing the world for the better one website at a time.
+	            We are experienced developers based out of Shenandoah, Virginia, with a fiery passion for creating web solutions, helping others, and saving the planet.
+	            Changing the world for the better, one website at a time, plus giving 10% of all sales to help protect our beautiful Earth.
             </p>
-    </div>
-</div>
-<footer>
-<div class="wrapper">
-    <small>&copy;2021 <strong>Aevi Web LLC</strong>, All Rights Reserved</small>
-    <nav class="footer-nav">
-        <a href="#">Back to Top</a>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Pricing</a>
-        <a href="#">Contact</a>
-    </nav>
-</div>
-</footer>
-<!-- jQuery first, then Popper.js, then Bootstrap JS. -->
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+	    </div>
+	</div>
+	
+	<!-- Footer -->
+	<?php include_once 'footer.php' ?>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS. -->
+	<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
