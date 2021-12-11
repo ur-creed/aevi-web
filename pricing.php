8221cc5fc287553e793da7b9398bb318c269c62b
Chris Polen <polen.chris89@gmail.com>
2021-12-10 22:17:57 -0500

    added images to cards. prices, styled button(temp) styled form

--- pricing.php
+++ pricing.php
@@ -36,25 +36,31 @@
         <div class="row row-cols-1 row-cols-md-2 g-4">
             <div class="col">
                 <div class="card">
-                    <img src="images/bluebg.jpeg" class="card-img-top" alt="...">
+                    <img src="images/newproject.jpeg" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
                         <h5 class="display-3 card-title">New Project</h5>
                         <p class="card-text lead">Looking to start a brand new project? Whether you know exactly what you want or have absolutely no idea where to begin, We can help with that!</p>
-	                    <button id="NewProjectButton" type="button">
-		                    Learn more
-	                    </button>
+                        <p class="card-text"><span class="badge bg-primary">$750</span></p>
+	                    <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-primary " id="NewProjectButton" type="button">
+                                Learn more
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                    <img src="images/bluebg.jpeg" class="card-img-top" alt="...">
+                    <img src="images/maintenance.jpeg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="display-3 card-title">Maintenance</h5>
-                        <p class="card-text">Have an existing website that needs updated information or just flat out needs to take that leap to the future? You're in the right place to teleport it there!</p>
-	                    <button id="ExistingProjectButton" type="button">
-		                    Learn more
-	                    </button>
+                        <p class="card-text lead">Have an existing website that needs updated information or just flat out needs to take that leap to the future? You're in the right place to teleport it there!</p>
+                        <p class="card-text"><span class="badge bg-primary">$750</span></p>
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-primary" id="ExistingProjectButton" type="button">
+                                Learn more
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -65,18 +71,21 @@
 	<div class="container container-form">
         <div class="row row-content">
             <form id="PricingContactForm">
+                <div class="secondaryTitle title">
+                    Please fill out this form below.
+                </div>
                 <div class="mb-3">
                     <label class="form-label" for="FullName">First and last name</label>
-                    <input id="FullName" name="full_name" class="form-control" type="text" placeholder="Ragnar Doe" required>
+                    <input id="FullName" name="full_name" class="form-control name formEntry" type="text" placeholder="Ragnar Doe" required>
                 </div>
                 <div class="mb-3">
                     <label class="form-label" for="ClientEmail">Email address</label>
-                    <input id="ClientEmail" name="client_email" class="form-control" type="email" placeholder="ragnar@email.com" aria-describedby="EmailHelp" required>
+                    <input id="ClientEmail" name="client_email" class="form-control email formEntry" type="email" placeholder="ragnar@email.com" aria-describedby="EmailHelp" required>
                     <span id="EmailHelp" class="form-text text-white">We'll never share your email with anyone else!</span>
                 </div>
                 <div class="mb-3">
                     <label class="form-label" for="PackageSelect">Choose a package:</label>
-                    <select id="PackageSelect" name="package_select" class="form-select" required>
+                    <select id="PackageSelect" name="package_select" class="form-select select formEntry" required>
                         <option value="">Select an option</option>
                         <option value="new">New project</option>
                         <option value="maintenance">Maintenance</option>
@@ -84,9 +93,11 @@
                 </div>
                 <div class="mb-3">
                     <label class="form-label" for="Message">A little about your dream</label>
-                    <textarea id="Message" name="message" class="form-control" cols="30" rows="10" placeholder="Tell us a little about the project you want built..." required></textarea>
+                    <textarea id="Message" name="message" class="form-control message formEntry" cols="30" rows="10" placeholder="Tell us a little about the project you want built..." required></textarea>
                 </div>
-                <button class="btn btn-lg btn-outline-success" id="SubmitInquiryButton" type="submit" form="ContactForm">Submit inquiry</button>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-outline-success" id="SubmitInquiryButton" type="submit" form="ContactForm">Submit</button>
+                </div>
             </form>
         </div>
     </div>
