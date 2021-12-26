1cf955a309d8bd949557cdcad7f3645cc0ea3a4f
Chris Polen <polen.chris89@gmail.com>
2021-12-26 17:32:29 -0500

    footer comment out delete

--- footer.php
+++ footer.php
@@ -1,14 +1,4 @@
-<!--<footer>-->
-<!--	<div class="wrapper1">-->
-<!--		<small>&copy;2021 <strong>Aevi Web LLC</strong>, All Rights Reserved</small>-->
-<!--		<nav class="footer-nav">-->
-<!--			<a href="index.php">Home</a>-->
-<!--			<a href="about.php">About</a>-->
-<!--			<a href="pricing.php">Pricing</a>-->
-<!--			<a href="contact.php">Contact</a>-->
-<!--		</nav>-->
-<!--	</div>-->
-<!--</footer>-->
+1
 <footer>
 
         <div class="container">
@@ -28,7 +18,4 @@
                 </div>
             </div>
         </div>
-
-
-
 </footer>
