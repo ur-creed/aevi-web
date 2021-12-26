c1d446cd93902eba2af44088eb63c52d6c8e5ceb
Chris Polen <polen.chris89@gmail.com>
2021-12-26 16:20:48 -0500

    footer? all pages updated

--- footer.php
+++ footer.php
@@ -9,26 +9,26 @@
 <!--		</nav>-->
 <!--	</div>-->
 <!--</footer>-->
-<footer class="text-center text-white" style="background: rgb(245, 245, 245);">
-<!-- Grid Container -->
-    <div class="container p-4">
+<footer>
 
-
-
-
-
-    </div>
-    <div class="text-center p-3 copyright">
-        @ 2021 Copyright:
-        <a href="#" class="aeviweb">Aeviweb.com</a>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <span class="divider">|</span>
-            <li><a href="about.php">About</a></li>
-            <span class="divider">|</span>
-            <li><a href="pricing.php">Pricing</a></li>
-            <span class="divider">|</span>
-            <li><a href="contact.php">Contact</a></li>
-        </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-sx-12">
+                    <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by
+                            <a href="index.php">Aevi Web</a>
+                    </p>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="foot-links">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="pricing.php">Pricing</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
+
+
 </footer>
