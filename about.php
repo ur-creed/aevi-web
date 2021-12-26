5dfa81d7ebbb6c2a99351e027a484cf79c4b6626
Chris Polen <polen.chris89@gmail.com>
2021-12-26 17:45:13 -0500

    pipes white

--- about.php
+++ about.php
@@ -62,7 +62,7 @@
                     <div class="col-md-6">
                         <div class="row align-items-center h-100">
                             <div class="col">
-                                <h1 class="display-3 text-black">Creed Polen | Chief Executive Officer</h1>
+                                <h1 class="display-3 text-black">Creed Polen <span class="divider">|</span> Chief Executive Officer</h1>
                                 <p class="lead text-white founderP">Some Info will go here............alksdjflk;djsfa l;fdjasdk;fjskfdkfkdsfja;dksfj
                                     al;kdjas;kdfjasdk;fljasdkfdf;f;fjfkdfj;fsd;f;l
                                     <br /><br />
@@ -83,7 +83,7 @@
                     <div class="col-md-6 order-md-6">
                         <div class="row align-items-center h-100">
                             <div class="col">
-                                <h1 class="display-3 text-black">Chris Polen | Chief Design Officer</h1>
+                                <h1 class="display-3 text-black">Chris Polen <span class="divider">|</span> Chief Design Officer</h1>
                                 <p class="lead text-white founderP">Some Info will go here............alksdjflk;djsfa l;fdjasdk;fjskfdkfkdsfja;dksfj
                                     al;kdjas;kdfjasdk;fljasdkfdf;f;fjfkdfj;fsd;f;l
                                     <br /><br />
