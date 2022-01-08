5759e0e471dce10e7ea7ce88b6993637ee3cf3c8
Chris Polen <polen.chris89@gmail.com>
2022-01-08 12:12:19 -0500

    Favicon

--- pricing.php
+++ pricing.php
@@ -17,6 +17,11 @@
     <link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
     <!-- Local CSS -->
     <link rel="stylesheet" href="css/pricing.css" type="text/css">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
 <body>
 	<!-- Navigation -->
 	<?php include_once 'navigation.php' ?>
