fd02cc3cb3735dea51554c4771c6d6957637a618
Creed <creed@aeviweb.com>
2022-01-13 15:42:16 -0500

    Initial commit

---
+++ portfolio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Aevi Web | Portfolio</title>
+	<meta name="description" content="A simple HTML5 Template for new projects.">
+	<meta name="author" content="SitePoint">
+	
+	<meta property="og:title" content="A Basic HTML5 Template">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+	<meta property="og:description" content="A simple HTML5 Template for new projects.">
+	<meta property="og:image" content="image.png">
+	
+	<link rel="stylesheet" href="css/portfolio.css">
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="manifest" href="favicon/site.webmanifest">
+</head>
+<body>
+	<!-- Navigation -->
+	<?php include_once 'navigation.php' ?>
+	<!-- Particles -->
+	<?php include_once 'particles.php' ?>
+	<div class="image-wrapper">
+		<img class="image-1" src="images/clients/dare_angel_logo_new.png" height="500" width="500" alt="Dare Angel Productions"/>
+		<a href="https://dareangel.com">Dare Angel</a>
+	</div>
+</body>
+</html>
