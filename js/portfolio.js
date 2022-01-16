143e85d2cad487ae8a2a457bd0eacc1341d90193
Creed <creed@aeviweb.com>
2022-01-16 11:14:00 -0500

    Fixed variable initialization

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,6 +2,7 @@ let $portfolioNavLink;
 
 // Initial page load
 $(document).ready(function(){
+    $portfolioNavLink = $('#Portfolio');
     // Make the current page active.
     $portfolioNavLink.addClass('active');
 });
