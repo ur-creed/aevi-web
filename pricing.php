ebd8e4103fce0fe40b0fa6648deac8bc16516ad1
Creed <creed@aeviweb.com>
2023-09-09 13:25:21 -0400

    Updated pricing to services.

--- pricing.php
+++ pricing.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aevi Web | Pricing</title>
+    <title>Aevi Web | Services</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Archivo" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
